4fc3b35ee3589a8172a69d87ef36631e1d36aa0f
Ivan Straka <is.straka@gmail.com>
2025-02-12 17:35:42 +0100

    [WFSSL-119] Update SslCiphersTest - TLS_AES_128_CCM_8_SHA256 is disabled in RHEL

--- java/src/test/java/org/wildfly/openssl/SslCiphersTest.java
+++ java/src/test/java/org/wildfly/openssl/SslCiphersTest.java
@@ -132,7 +132,7 @@ public class SslCiphersTest extends AbstractOpenSSLTest {
                 "TLS_AES_256_GCM_SHA384",
                 "TLS_CHACHA20_POLY1305_SHA256",
                 "TLS_AES_128_GCM_SHA256",
-                "TLS_AES_128_CCM_8_SHA256",
+                //"TLS_AES_128_CCM_8_SHA256",
                 "TLS_AES_128_CCM_SHA256"
         };
 
